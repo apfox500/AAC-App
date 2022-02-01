e0b294ff7d320d961291695da830168941197d57
apfox500 <apfox500@gmail.com>
2022-02-01 09:36:57 -0700

    Added the base layout(no functionality)
    Just a warning that i was sing macOS to improve runtime, so might not be perfect spacing for ipads.

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      //TODO: Add in a title here
       title: 'Flutter Demo',
       theme: ThemeData(
         // This is the theme of your application.
@@ -24,7 +25,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      //TODO: Add in a better title for the page
+      home: const MyHomePage(title: 'Home Page'),
     );
   }
 }
@@ -57,35 +59,144 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[
-          Container(
-            child: Text("Objects")
-          ),
-          Row(children: <Widget>[
-            Column(
-              children: [
-                Container(
-                  child: const Text("Actions"),
+        appBar: AppBar(
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: Text(widget.title),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              //Read aloud text
+              Container(
+                width: MediaQuery.of(context).size.width * .9,
+                height: MediaQuery.of(context).size.height * .15,
+                child: const Center(
+                    child: Text("Text to be read aloud will go here")),
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.blue,
+                  ),
+                  borderRadius: BorderRadius.circular(10.0),
                 ),
-                Container(
-                  child: const Text("Common Sentences"),
-                )
-              ],
-            ),
-            Column(
-              children: [
-                Container(child: const Text("Pronouns"),)
-              ],
-            )
-          ],),
-        ],
-      ) // This trailing comma makes auto-formatting nicer for build methods.
-    );
+              ),
+              //padding
+              const SizedBox(
+                height: 25,
+              ),
+              //Objects
+              Container(
+                width: MediaQuery.of(context).size.width * .9,
+                height: MediaQuery.of(context).size.height * .15,
+                child:
+                    TextButton(onPressed: () {}, child: const Text("Objects")),
+                decoration: BoxDecoration(
+                  color: Colors.purple.shade200,
+                  borderRadius: BorderRadius.circular(10.0),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.5),
+                      spreadRadius: 5,
+                      blurRadius: 7,
+                      offset: const Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+              ),
+              //padding
+              const SizedBox(
+                height: 25,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      //Actions
+                      Container(
+                        width: MediaQuery.of(context).size.width * .44,
+                        height: MediaQuery.of(context).size.height * .15,
+                        child: TextButton(
+                            onPressed: () {}, child: const Text("Actions")),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10.0),
+                          //TODO: Get the gradient to work, it refuses to work and go diagonal but oh well
+                          gradient: LinearGradient(
+                              colors: [
+                                Colors.pink.shade300,
+                                Colors.pink.shade200
+                              ],
+                              begin: const Alignment(0, 0),
+                              end: Alignment.bottomLeft),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: const Offset(
+                                  0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                      ),
+                      //Padding
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      //Common Sentences
+                      Container(
+                        width: MediaQuery.of(context).size.width * .44,
+                        height: MediaQuery.of(context).size.height * .15,
+                        child: TextButton(
+                            onPressed: () {},
+                            child: const Text("Common Sentences")),
+                        decoration: BoxDecoration(
+                          color: Colors.lightGreen,
+                          borderRadius: BorderRadius.circular(10.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.5),
+                              spreadRadius: 5,
+                              blurRadius: 7,
+                              offset: const Offset(
+                                  0, 3), // changes position of shadow
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                  //Padding
+                  const SizedBox(width: 25),
+                  //Pronouns
+                  Container(
+                    width: MediaQuery.of(context).size.width * .43,
+                    height: 225,
+                    child: TextButton(
+                        onPressed: () {}, child: const Text("Pronouns")),
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(10.0),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.5),
+                          spreadRadius: 5,
+                          blurRadius: 7,
+                          offset:
+                              const Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }

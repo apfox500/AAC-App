56181941a78077424e7292aae679c4e82ad071cd
apfox500 <apfox500@gmail.com>
2022-02-01 13:47:59 -0700

    Got the fimga sketches updated pretty much
    needs lots of work still

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+//TODO: Fix slight bug where this wont update across screens(some kind of varaible to pass maybe or a set state call?)
+String _readAloudText = "";
 void main() {
   runApp(const MyApp());
 }
@@ -64,8 +66,12 @@ class _MyHomePageState extends State<MyHomePage> {
           // the App.build method, and use it to set our appbar title.
           title: Text(widget.title),
         ),
+        //Button to make it read aloud? not sure just copying the figma
+        //TODO: Implement functionality for this floatingActionButton
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
+          heroTag: 'readaloudbtn',
+          backgroundColor: Colors.grey,
         ),
         body: Center(
           child: Column(
@@ -75,8 +81,7 @@ class _MyHomePageState extends State<MyHomePage> {
               Container(
                 width: MediaQuery.of(context).size.width * .9,
                 height: MediaQuery.of(context).size.height * .15,
-                child: const Center(
-                    child: Text("Text to be read aloud will go here")),
+                child: Center(child: Text(_readAloudText)),
                 decoration: BoxDecoration(
                   border: Border.all(
                     color: Colors.blue,
@@ -153,7 +158,14 @@ class _MyHomePageState extends State<MyHomePage> {
                         width: MediaQuery.of(context).size.width * .44,
                         height: MediaQuery.of(context).size.height * .15,
                         child: TextButton(
-                            onPressed: () {},
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const CommonSentencesPage()),
+                              );
+                            },
                             child: const Text("Common Sentences")),
                         decoration: BoxDecoration(
                           color: Colors.lightGreen,
@@ -198,5 +210,234 @@ class _MyHomePageState extends State<MyHomePage> {
             ],
           ),
         ));
+  }
+}
+
+class CommonSentencesPage extends StatefulWidget {
+  const CommonSentencesPage({Key? key}) : super(key: key);
+
+  @override
+  _CommonSentencesPageState createState() => _CommonSentencesPageState();
+}
+
+class _CommonSentencesPageState extends State<CommonSentencesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Common Sentences"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            //Read aloud text
+            Container(
+              width: MediaQuery.of(context).size.width * .9,
+              height: MediaQuery.of(context).size.height * .08,
+              child: Center(child: Text(_readAloudText)),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.blue,
+                ),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            //padding
+            const SizedBox(
+              height: 25,
+            ),
+            //My name is
+            Container(
+              width: MediaQuery.of(context).size.width * .9,
+              height: MediaQuery.of(context).size.height * .08,
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    _readAloudText = "Hello, My Name is...";
+                  });
+                },
+                child: const Text("Hello, My Name is..."),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.yellow.shade200,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+            ),
+            //padding
+            const SizedBox(
+              height: 25,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    //Goodbye
+                    Container(
+                      width: MediaQuery.of(context).size.width * .44,
+                      height: MediaQuery.of(context).size.height * .08,
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _readAloudText = "Goodbye";
+                          });
+                        },
+                        child: const Text("Goodbye"),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple.shade200,
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(
+                                0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                    //padding
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    //Lorem Ipsum
+                    Container(
+                      width: MediaQuery.of(context).size.width * .44,
+                      height: MediaQuery.of(context).size.height * .3,
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _readAloudText = "Lorem Ipsum";
+                          });
+                        },
+                        child: const Text("Lorem Ipsum"),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.redAccent.shade100,
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(
+                                0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(
+                  width: 25,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    //Good Job
+                    Container(
+                      width: MediaQuery.of(context).size.width * .43,
+                      height: MediaQuery.of(context).size.height * .3,
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _readAloudText = "Good Job";
+                          });
+                        },
+                        child: const Text("Good Job"),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.redAccent.shade100,
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(
+                                0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                    //padding
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    //I'm Hungry
+                    Container(
+                      width: MediaQuery.of(context).size.width * .44,
+                      height: MediaQuery.of(context).size.height * .08,
+                      child: TextButton(
+                        onPressed: () {
+                          setState(() {
+                            _readAloudText = "I am Hungry";
+                          });
+                        },
+                        child: const Text("I am Hungry"),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepPurple.shade200,
+                        borderRadius: BorderRadius.circular(10.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5),
+                            spreadRadius: 5,
+                            blurRadius: 7,
+                            offset: const Offset(
+                                0, 3), // changes position of shadow
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                )
+              ],
+            ),
+            //padding
+            const SizedBox(
+              height: 25,
+            ),
+            //I don't know
+            Container(
+              width: MediaQuery.of(context).size.width * .9,
+              height: MediaQuery.of(context).size.height * .08,
+              child: TextButton(
+                onPressed: () {
+                  setState(() {
+                    _readAloudText = "I don't know";
+                  });
+                },
+                child: const Text("I don't know"),
+              ),
+              decoration: BoxDecoration(
+                color: Colors.lightBlue.shade100,
+                borderRadius: BorderRadius.circular(10.0),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: 5,
+                    blurRadius: 7,
+                    offset: const Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
